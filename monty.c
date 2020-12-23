18f9a1b09f83d0462e2c30f28f1c2e31bb82a4e4
Xvaria <2349@holbertonschool.com>
2020-12-23 02:15:38 +0000

    Ready main script

---
+++ monty.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * main - opens a monty script file
+ * @argc: count
+ * @argv: array
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	analyze_file(file);
+	return (EXIT_SUCCESS);
+}
